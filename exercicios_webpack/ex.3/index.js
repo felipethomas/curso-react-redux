514ec5b57a898cd03fbdded3e0312f09a0c19a0d
Felipe <felipecejug@gmail.com>
2018-08-03 15:28:41 -0300

    ajustes nos exercícios

--- exercicios_webpack/ex.3/index.js
+++ exercicios_webpack/ex.3/index.js
@@ -1,1 +1,3 @@
-require('./duvidaCruel')
+console.log("Existe uma pergunta abaixo que somente aparece se o js for referenciado para integrar o bundle.js")
+//require('./duvidaCruel') 
+console.log("Existe uma pergunta acima.")
