4d34ad41dd1fb621226a35d451626f802b546ec6
Felipe Andrade <felipe.andrade@serpro.gov.br>
2018-08-22 13:58:08 -0300

    exercicios

---
+++ todo-app/frontend/src/todo/todo.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
+
+export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({ ...this.state, description: e.target.value })
+    }
+
+    handleAdd() {
+        const description = this.state.description
+        axios.post(URL, { description })
+            .then(resp => console.log('Funcionou!'))
+    }
+
+    render() {
+        return (
+            <div>
+                <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
+                <TodoForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
+                <TodoList />
+            </div>
+        )
+    }
+}
