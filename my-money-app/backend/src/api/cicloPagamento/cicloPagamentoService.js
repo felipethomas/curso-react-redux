af0d043b151463f0564eec3656de424a2649f756
Felipe Thomas <felipe.andrade@serpro.gov.br>
2019-03-12 15:00:21 -0300

    Implementação do backend do projeto my-money

--- my-money-app/backend/src/api/cicloPagamento/cicloPagamentoService.js
+++ my-money-app/backend/src/api/cicloPagamento/cicloPagamentoService.js
@@ -1,7 +1,9 @@
 const CicloPagamento = require('./cicloPagamento')
+const errorHandler = require('../common/errorHandler')
 
 CicloPagamento.methods(['get', 'post', 'put', 'delete'])
 CicloPagamento.updateOptions({ new: true, runValidators: true })
+CicloPagamento.after('post', errorHandler).after('put', errorHandler)
 
 CicloPagamento.route('count', (req, res, next) => {
     CicloPagamento.count((error, value) => {
@@ -18,7 +20,7 @@ CicloPagamento.route('summary', (req, res, next) => {
         .project({ credito: { $sum: "$creditos.valor" }, debito: { $sum: "$debitos.valor" } })
         .group({ _id: null, credito: { $sum: "$credito" }, debito: { $sum: "$debito" } })
         .project({ _id: 0, credito: 1, debito: 1 })
-        .cursor({ batchSize: 100 })
+        .cursor()
         .exec()
 
     cursor.get((error, result) => {
@@ -28,21 +30,6 @@ CicloPagamento.route('summary', (req, res, next) => {
             res.json(result[0] || { credito: 0, debito: 0 })
         }
     })
-
-    // Implementação anterior no curso
-    // CicloPagamento.aggregate([{
-    //     $project: { credito: { $sum: "$creditos.valor" }, debito: { $sum: "$debitos.valor" } }
-    // }, {
-    //     $group: { _id: null, credito: { $sum: "$credito" }, debito: { $sum: "$debito" } }
-    // }, {
-    //     $project: { _id: 0, credito: 1, debito: 1 }
-    // }], (error, result) => {
-    //     if (error) {
-    //         res.status(500).json({ errors: [error] })
-    //     } else {
-    //         res.json(result[0] || { credito: 0, debito: 0 })
-    //     }
-    // })
 })
 
 module.exports = CicloPagamento
